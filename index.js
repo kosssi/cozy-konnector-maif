2d63e79c360415c2b489d05af9c5e3b4584ad888
Travis CI User <travis@example.org>
2017-09-01 14:48:15 +0000

    publish: feat: log cozyfield for production debug :speaker:
    generated from commit 080eafbd74ca5f1a8e12381520557ee91c606706

--- index.js
+++ index.js
@@ -63701,6 +63701,7 @@ const cozyFields = JSON.parse(process.env.COZY_FIELDS)
 log('info', 'cozyfields', cozyFields)
 
 konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
+  log('error', 'cozyfields', cozyFields)
   log('debug', 'The konnector has been run')
   if (err) {
     log('error', err)
